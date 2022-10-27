5e4969ed32316c8929e5950b0167dd0fe9f88f00
Filipe Campos <103011013+FilipeCamposFDZ@users.noreply.github.com>
2022-10-26 21:45:15 -0300

    Resolvendo Cors 03

--- src/app.js
+++ src/app.js
@@ -7,11 +7,10 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-    app.use(cors());
     next();
 })
 
